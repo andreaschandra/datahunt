b9e60bc5acace57aea67daa3fb3045575f5ddba9
Andreas Chandra <phoenix.ichigo@yahoo.com>
2022-12-18 23:52:09 +0700

    0.2.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="datahunt",
     packages=find_packages(),
-    version="0.1.0",
+    version="0.2.0",
     license="MIT",
     description="Python library for testing data science skills",
     author="Andreas Chandra",
